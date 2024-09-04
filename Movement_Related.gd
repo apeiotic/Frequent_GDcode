a865d8d38a02b27fc43a0f63d969631a5f6a5071
Prajyot Kumar <130589812+apeiotic@users.noreply.github.com>
2024-09-04 15:13:32 +0530

    Update Movement_Related.gd

--- Movement_Related.gd
+++ Movement_Related.gd
@@ -1,4 +1,4 @@
-#movement System
+# Default/simple movement System
 var input_dir := Input.get_vector("Left", "Right", "Up", "Down")
 var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 if direction:
@@ -7,6 +7,31 @@ if direction:
 else:
 	velocity.x = move_toward(velocity.x, 0, SPEED)
 	velocity.z = move_toward(velocity.z, 0, SPEED)
+
+
+
+
+
+#Slippry Movement system
+
+var input_dir = Input.get_vector("Left", "Right", "Up", "Down")
+	
+var camera_basis = camera.global_transform.basis
+var direction = camera_basis * Vector3(input_dir.x, 0, input_dir.y)
+direction = direction.normalized()
+if direction:
+	velocity.x = direction.x * SPEED
+	velocity.z = direction.z * SPEED
+	walking=true
+else:
+	# Lerp towards 0 velocity
+	var lerp_amount = 0.05 # Adjust this value for smoother/faster deceleration
+	velocity.x = lerp(velocity.x, 0.0, lerp_amount)
+	velocity.z = lerp(velocity.z, 0.0, lerp_amount)
+	walking=false
+
+
+
 
 
 #Camera Movement 
