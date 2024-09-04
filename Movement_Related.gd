618216e228fc85171fffd13dbb3ab4711b1f9429
Prajyot Kumar <130589812+apeiotic@users.noreply.github.com>
2024-09-04 15:07:49 +0530

    Create Movement_Related.gd
    Movement related codes

---
+++ Movement_Related.gd
@@ -0,0 +1,9 @@
+#movement System
+	var input_dir := Input.get_vector("Left", "Right", "Up", "Down")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
