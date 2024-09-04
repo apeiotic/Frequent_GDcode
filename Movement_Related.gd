ae3a0352d8b8b11ca627a5b87030c242abbd2432
Prajyot Kumar <130589812+apeiotic@users.noreply.github.com>
2024-09-04 15:10:07 +0530

    Update Movement_Related.gd

--- Movement_Related.gd
+++ Movement_Related.gd
@@ -1,9 +1,21 @@
 #movement System
-	var input_dir := Input.get_vector("Left", "Right", "Up", "Down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+var input_dir := Input.get_vector("Left", "Right", "Up", "Down")
+var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+if direction:
+	velocity.x = direction.x * SPEED
+	velocity.z = direction.z * SPEED
+else:
+	velocity.x = move_toward(velocity.x, 0, SPEED)
+	velocity.z = move_toward(velocity.z, 0, SPEED)
+
+
+#Camera Movement 
+var sensitivity = 0.05
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * sensitivity)
+		camera.rotate_x(-event.relative.y * sensitivity)
