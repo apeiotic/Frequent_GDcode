eeb8d7661ec18e6abd0f7301c37f1508c5776368
Prajyot Kumar <130589812+apeiotic@users.noreply.github.com>
2024-09-04 15:17:08 +0530

    Created Important_Code.gd

---
+++ Important_Code.gd
@@ -0,0 +1,8 @@
+#Quiting 
+func _process(delta):
+	if Input.is_action_just_pressed("Quit"): #("Quit" is an input set from project settings)
+		get_tree().quit()
+
+#For restarting the current Better for quick checking
+  if Input.is_action_just_pressed("Restart"): #("Restart" is an input set from project settings)
+    get_tree().reload_current_scene()
